4a4a00018587f953f8fd8828d6a0b7506830ee97
AFontanMoreira <afontanmoreira@danielcastelao.org>
2021-06-01 20:56:51 +0200

    Comando /help

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,43 @@
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.ParseMode
+
+
 fun main(args: Array<String>) {
     println("hola mundo")
+
+    val bot = bot {
+        token = Apikey.mitoken
+
+        dispatch {
+
+            /**
+             * Comando /help
+             *
+             * Descripcion y listado de los comandos
+             */
+
+            command("help"){
+
+                val help = """
+                    */saludo* | Javi se presenta
+                    */justificante* | Envia justificante
+                    */desaparecer* | Javi desaparece de clase
+                    */excusa* | Genera una escusa para un nuevo dia
+                    """.trimIndent()
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = help,
+                    parseMode = ParseMode.MARKDOWN
+                )
+            }
+
+
+        }
+
+    }
+    bot.startPolling()
 }
