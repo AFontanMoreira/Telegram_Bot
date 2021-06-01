0b405ad97615eb6c2b18eb62a9a76bcc2b87c69b
AFontanMoreira <afontanmoreira@danielcastelao.org>
2021-06-01 21:58:57 +0200

    Comando /justificante

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,13 @@ import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.entities.ParseMode
+import com.sun.xml.internal.fastinfoset.util.StringArray
 
 
 fun main(args: Array<String>) {
     println("hola mundo")
+
+
 
     val bot = bot {
         token = Apikey.mitoken
@@ -51,8 +54,19 @@ fun main(args: Array<String>) {
              */
 
             command("desaparecer"){
-                val respuestaDesaparecer = bot.sendMessage(chatId = ChatId.fromId(update.message!!.chat.id), text = "Espera que hago en clase, Me largooooo!!!!")
+                val respuestaDesaparecer = bot.sendMessage(chatId = ChatId.fromId(update.message!!.chat.id), text = "Espera que hago en clase. Me largooooo!!!!")
                 val respuestaDesaparecer2 = bot.sendAnimation(chatId = ChatId.fromId(update.message!!.chat.id), "https://media.tenor.com/images/83da5b25bd710dda7ce487ee322895c1/tenor.gif" )
+            }
+
+            /**
+             *Comando /justificante
+             *
+             * Envio de un justificante para faltar a clase
+             */
+
+            command("justificante"){
+                val respuestaJustificante = bot.sendMessage(chatId = ChatId.fromId(update.message!!.chat.id), text = "Aqui tiene su justificante para poder faltar hoy. Imprimalo las veces que lo necesite.")
+                val respuestaJustificante2 = bot.sendPhoto(chatId = ChatId.fromId(update.message!!.chat.id), "https://loentiendo.com/wp-content/uploads/2015/11/nuevo-sistema-bajas-medicas.jpg")
             }
 
         }
